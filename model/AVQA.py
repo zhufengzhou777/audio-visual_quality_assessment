fda3d8dc928b030ad1983f0b18767259e0a65a68
zfz <2049325941@qq.com>
2023-02-08 15:47:38 +0800

    Update AVQA.py

--- model/AVQA.py
+++ model/AVQA.py
@@ -6,20 +6,18 @@ from model.vit_feature_extractor import MLP
 
 
 class AVQA(nn.Module):
-    def __init__(self, dim=768, fusion_dim=32, length=24):
+    def __init__(self, dim=768, fusion_dim=512, length=24):
         super().__init__()
         self.attn_layer1 = FusedSTBlock(dim=dim, num_heads=8)
         self.attn_layer2 = FusedSTBlock(dim=dim, num_heads=8)
         self.mlp1 = MLP(in_features=dim)
         self.mlp2 = MLP(in_features=dim)
-        self.regressor1 = MLP(in_features=dim, hidden_features=dim // 2, out_features=1)
-        self.regressor2 = MLP(in_features=dim, hidden_features=dim // 2, out_features=1)
-        self.anormal = nn.LayerNorm(197)
-        self.vnormal = nn.LayerNorm(197)
+        self.anormal = nn.LayerNorm(768)
+        self.vnormal = nn.LayerNorm(768)
         self.tanh = nn.Tanh()
         self.relu = nn.ReLU()
-        self.aencoder = nn.Linear(197, fusion_dim)
-        self.vencoder = nn.Linear(197, fusion_dim)
+        self.aencoder = nn.Linear(768, fusion_dim)
+        self.vencoder = nn.Linear(768, fusion_dim)
         self.adrop = nn.Dropout()
         self.vdrop = nn.Dropout()
         self.Wj_a = nn.Linear(length, length, bias=False)
@@ -30,14 +28,14 @@ class AVQA(nn.Module):
         self.W_cv = nn.Linear(fusion_dim * 2, fusion_dim, bias=False)
         self.W_ha = nn.Linear(fusion_dim, length, bias=False)
         self.W_hv = nn.Linear(fusion_dim, length, bias=False)
-        self.score = nn.Sequential(nn.LayerNorm(1536),
-                                   nn.Linear(1536, 512),
+        self.score = nn.Sequential(nn.LayerNorm(24),
+                                   nn.Linear(24, 32),
                                    nn.ReLU(),
                                    nn.Dropout(),
-                                   nn.Linear(512, 128),
+                                   nn.Linear(32, 16),
                                    nn.ReLU(),
                                    nn.Dropout(),
-                                   nn.Linear(128, 1))
+                                   nn.Linear(16, 1))
         self._initialize_weights()
 
     def _initialize_weights(self):
@@ -52,7 +50,7 @@ class AVQA(nn.Module):
         vst_feat = self.attn_layer1(vst_token)  # [B,F,196,768]
         vglobal_feat = self.mlp1(vcls_token)  # [B,F,1,768]
         visual_feat = torch.cat((vst_feat, vglobal_feat), dim=2)  # [B,F,197,768]
-        visual_feat = torch.squeeze(self.regressor1(visual_feat))  # [B,F,197]
+        visual_feat = torch.squeeze(visual_feat.mean(-2))  # [B,F,197]
 
         # audio-fused-st
         acls_token = audio[:, :, 0, :].unsqueeze(2)  # [B,F,1,768]
@@ -60,7 +58,7 @@ class AVQA(nn.Module):
         ast_feat = self.attn_layer2(ast_token)  # [B,F,196,768]
         aglobal_feat = self.mlp2(acls_token)  # [B,F,1,768]
         audio_feat = torch.cat((ast_feat, aglobal_feat), dim=2)  # [B,F,197,768]
-        audio_feat = torch.squeeze(self.regressor2(audio_feat))  # [B,F,197]
+        audio_feat = torch.squeeze(audio_feat.mean(-2))  # [B,F,197]
 
         audio_feat = self.anormal(audio_feat)
         visual_feat = self.vnormal(visual_feat)
@@ -80,6 +78,6 @@ class AVQA(nn.Module):
         Xatt_a = self.W_ha(H_a.transpose(1, 2)).transpose(1, 2) + X_a
         Xatt_v = self.W_hv(H_v.transpose(1, 2)).transpose(1, 2) + X_v
         X_att = torch.cat((Xatt_v, Xatt_a), dim=2)
-        feature = X_att.flatten(1)
+        feature = X_att.mean(-1)
         score = self.score(feature)
         return score
